ff606623276ec464c593218ed0b6053670281181
Misheck Gogo <136022287+MisheckGalx@users.noreply.github.com>
2026-03-31 12:50:10 +0200

    Update hero.js

--- public/js/hero.js
+++ public/js/hero.js
@@ -65,7 +65,7 @@
         <h1>
           Engineering<br>
           <span class="accent">Excellence</span>
-          <span class="indent">in E&amp;E Solutions</span>
+          <span class="indent">in Eamp;EE Solutions</span>
         </h1>
 
         <p class="hero-sub">
